82a9e7fa422ec323f81f1c3597508b043620d891
michael-de-wit <michael.b.dewit@gmail.com>
2025-10-28 11:58:04 -0700

    nucleotide match template-coding alignment enforcement

--- public/frontendbehavior.js
+++ public/frontendbehavior.js
@@ -1,13 +1,59 @@
 const nucleotideMatchCheckbox = document.querySelector(`#nucleotideMatchCheckbox`)
-const codingNucleotideInput = document.querySelector(`#codingNucleotideInput`)
+const templateNucleotide = document.querySelectorAll(`.templateNucleotide`)
+const codingNucleotide = document.querySelectorAll(`.codingNucleotide`)
+
+console.log(codingNucleotide);
+
+let RadioCodingNucleotideG = document.querySelector('input[id="codingNucleotideG"]');
+let RadioCodingNucleotideC = document.querySelector('input[id="codingNucleotideC"]');
+let RadioCodingNucleotideA = document.querySelector('input[id="codingNucleotideA"]');
+let RadioCodingNucleotideT = document.querySelector('input[id="codingNucleotideT"]');
 
 
-nucleotideMatchCheckbox.addEventListener('click', () => {
-    if(nucleotideMatchCheckbox.checked === false) {
-        codingNucleotideInput.readOnly = false
-        console.log(codingNucleotideInput.readonly);
-    } else {
-        codingNucleotideInput.readOnly = true
-        console.log(codingNucleotideInput.readonly);
-    };
+nucleotideMatchCheckbox.addEventListener('change', () => {
+    const selectedRadioTemplateNucleotide = document.querySelector('input[name="templateNucleotide"]:checked');
+    
+    if(nucleotideMatchCheckbox.checked === true && selectedRadioTemplateNucleotide.value == 'G') {
+        RadioCodingNucleotideC.checked = true
+    } else if(nucleotideMatchCheckbox.checked === true && selectedRadioTemplateNucleotide.value == 'C') {
+        RadioCodingNucleotideG.checked = true
+    } else if(nucleotideMatchCheckbox.checked === true && selectedRadioTemplateNucleotide.value == 'A') {
+        RadioCodingNucleotideT.checked = true
+    } else if(nucleotideMatchCheckbox.checked === true && selectedRadioTemplateNucleotide.value == 'T') {
+        RadioCodingNucleotideA.checked = true
+    }
+})
+
+templateNucleotide.forEach(radioButton => {
+    radioButton.addEventListener('change', () => {
+    const selectedRadioTemplateNucleotide = document.querySelector('input[name="templateNucleotide"]:checked');
+    console.log(selectedRadioTemplateNucleotide);
+
+    if(nucleotideMatchCheckbox.checked === true && selectedRadioTemplateNucleotide.value == 'G') {
+        RadioCodingNucleotideC.checked = true
+    } else if(nucleotideMatchCheckbox.checked === true && selectedRadioTemplateNucleotide.value == 'C') {
+        RadioCodingNucleotideG.checked = true
+    } else if(nucleotideMatchCheckbox.checked === true && selectedRadioTemplateNucleotide.value == 'A') {
+        RadioCodingNucleotideT.checked = true
+    } else if(nucleotideMatchCheckbox.checked === true && selectedRadioTemplateNucleotide.value == 'T') {
+        RadioCodingNucleotideA.checked = true
+    }
+    })
+})
+
+codingNucleotide.forEach(radioButton => {
+    radioButton.addEventListener('change', () => {
+    const selectedRadioTemplateNucleotide = document.querySelector('input[name="templateNucleotide"]:checked');
+    console.log(selectedRadioTemplateNucleotide);
+
+    if(nucleotideMatchCheckbox.checked === true && selectedRadioTemplateNucleotide.value == 'G') {
+        RadioCodingNucleotideC.checked = true
+    } else if(nucleotideMatchCheckbox.checked === true && selectedRadioTemplateNucleotide.value == 'C') {
+        RadioCodingNucleotideG.checked = true
+    } else if(nucleotideMatchCheckbox.checked === true && selectedRadioTemplateNucleotide.value == 'A') {
+        RadioCodingNucleotideT.checked = true
+    } else if(nucleotideMatchCheckbox.checked === true && selectedRadioTemplateNucleotide.value == 'T') {
+        RadioCodingNucleotideA.checked = true
+    }
+    })
 })
