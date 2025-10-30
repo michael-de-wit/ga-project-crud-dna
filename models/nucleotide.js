94c418a2b497771a71035a7cecfb723dd47a8d57
michael-de-wit <michael.b.dewit@gmail.com>
2025-10-30 15:34:35 -0700

    initial pass clean-up / documentation

--- models/nucleotide.js
+++ models/nucleotide.js
@@ -1,11 +1,13 @@
-const { default: mongoose } = require('mongoose');
+// MongoDB schema
+const { default: mongoose } = require('mongoose'); // Use the Mongoose library
 
+// Nucleotide pair schema for MongoDB
 const nucleotidePairsSchema = new mongoose.Schema({
 
-    nucleotidePosition: Number,
-    templateNucleotide: String,
-    codingNucleotide: String,
-    nucleotideMatch: Boolean,
+    nucleotidePosition: Number, // 1-inf
+    templateNucleotide: String, // G,C,A,T
+    codingNucleotide: String, // G,C,A,T
+    nucleotideMatch: Boolean, // True: G<>C, A<>T; otherwise False
     dateAdded: Date,
     dateModified: Date
 
